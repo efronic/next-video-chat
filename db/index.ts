77dc97b1f816c10211c80b92862f18bdf277f545
efron <eahmadifar@outlook.com>
2024-03-19 15:03:13 +1100

    Set up drizzle

---
+++ db/index.ts
@@ -0,0 +1,15 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+// import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import * as schema from './schema';
+import postgres from 'postgres';
+
+// for migrations
+// const migrationClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db", { max: 1 });
+// migrate(drizzle(migrationClient), ...)
+
+// for query purposes
+const queryClient = postgres(process.env.DATABASE_URL!, { max: 1 });
+const db = drizzle(queryClient, { schema });
+// await db.select().from(...)...
+
+export { db };
---
+++ db/schema.ts
@@ -0,0 +1,7 @@
+import { text, pgTable } from 'drizzle-orm/pg-core';
+
+
+export const testing = pgTable('testing', {
+  id: text('id').notNull().primaryKey(),
+  name: text('name'),
+});
