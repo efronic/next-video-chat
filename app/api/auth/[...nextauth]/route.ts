520256f908a433a7cb0e8fa7bafde127b4c329b6
efron <eahmadifar@outlook.com>
2024-04-16 22:44:37 +1000

    Fix an issue with DrizzleAdapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,19 @@
 import { db } from '@/db'
 import { PostgresJsDatabase } from 'drizzle-orm/postgres-js'
 import NextAuth from "next-auth"
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import GoogleProvider from "next-auth/providers/google"
+import { Adapter } from 'next-auth/adapters';
 
 const handler = NextAuth({
-  adapter: DrrizleAdapter(db),
+  adapter: DrizzleAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
 });
 
 export { handler as GET, handler as POST }
-
-function DrrizleAdapter(db: PostgresJsDatabase<typeof import("@/db/schema")>): import("next-auth/adapters").Adapter | undefined {
-    throw new Error('Function not implemented.')
-}
-function GoogleProvider(arg0: { clientId: string | undefined; clientSecret: string | undefined; }): import("next-auth/providers/index").Provider {
-    throw new Error('Function not implemented.');
-}
 
